188891fa9fee0a47220d2d86d037caa58f464cd1
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-06-23 17:17:37 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
